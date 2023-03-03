4137c784e64f6c68661e34c325e355cee3edf39f
Tanya <tanyakulemina94@gmail.com>
2023-03-03 19:14:41 +0300

    Homework

---
+++ 002/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int GetSum (int m, int n)
+{
+    if(m <= n) 
+        return m + GetSum (m+1, n);
+    else
+        return 0;
+}
+int m = 1;
+int n = 15;
+int sum = GetSum(m, n);
+Console.WriteLine (sum);
